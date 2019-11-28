7c6f7d1a2168aabde759b93b394d5c99493e06af
AhMeDz333 <ahmed.ashraf33@yahoo.com>
2019-11-28 16:09:34 +0200

    Added package.json file

--- step.sh
+++ step.sh
@@ -3,11 +3,8 @@ set -ex
 
 echo "* Changelog file path: $changelog_file_path"
 
-# Install node module dependencies
-npm install changelog-parser
-npm install child-process
-
 # Execute the extractor script
+echo "* Executing the release notes extractor"
 node release_notes_extractor.js $changelog_file_path
 
 exit 0
