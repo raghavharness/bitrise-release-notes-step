0c3a48932c081fe9898215ab04e355b52c66b5c1
AhMeDz333 <ahmed.ashraf33@yahoo.com>
2019-11-28 16:27:30 +0200

    Added keepMarkdownOutput control variable

--- release_notes_extractor.js
+++ release_notes_extractor.js
@@ -2,22 +2,32 @@ const parser = require('changelog-parser')
 const {execSync} = require('child_process')
 
 const outputEnvVar = "RELEASE_NOTES"
-let changelogFile = `${process.argv[2]}`
+let changelogFile = process.argv[2]
+let keepMarkdownOutput = process.argv[3].toLowerCase() == 'yes'
+
+console.log(keepMarkdownOutput)
 
 parseChangelog(changelogFile)
 
 function parseChangelog(filePath) {
     parser(filePath)
         .then(content => {
-            let releaseNotes = content['versions'][0]['body']
-            execSync(`envman add --key ${outputEnvVar} --value "${format(releaseNotes)}"`)
-            console.log(`* Wrote the environment variable ${outputEnvVar} successfully!`)
+            var releaseNotes = content['versions'][0]['body']
+            if (!keepMarkdownOutput) {
+                releaseNotes = trimChangeTypePrefixes(releaseNotes)
+            }
+            writeOutputEnvironmentVariable(releaseNotes)
         })
         .catch(err => {
             console.e(err)
         })
 }
 
-function format(releaseNotes) {
+function writeOutputEnvironmentVariable(releaseNotes) {
+    execSync(`envman add --key ${outputEnvVar} --value "${releaseNotes}"`)
+    console.log(`* Wrote the environment variable ${outputEnvVar} successfully!`)
+}
+
+function trimChangeTypePrefixes(releaseNotes) {
     return releaseNotes.replace(/^###\s?(.*)$/mg,"$1")
 }
--- step.sh
+++ step.sh
@@ -5,6 +5,6 @@ echo "* Changelog file path: $changelog_file_path"
 
 # Execute the extractor script
 echo "* Executing the release notes extractor"
-node release_notes_extractor.js $changelog_file_path
+node release_notes_extractor.js $changelog_file_path $keep_markdown_output
 
 exit 0
