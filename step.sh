912fc927a677ba293f67d12b0787eeab3e445107
ashrafDoubleO7 <ahmed.ashraf33@yahoo.com>
2019-11-29 14:45:30 +0200

    Fixed the trim markdown flag name and values

--- step.sh
+++ step.sh
@@ -2,10 +2,10 @@
 set -ex
 
 echo "* Changelog file path: $changelog_file_path"
-echo "* Keeping the markdown output: $keep_markdown_output"
 
 # Execute the extractor script
+echo "* $keep_markdown_output"
 echo "* Executing the release notes extractor"
-node release_notes_extractor.js $changelog_file_path $keep_markdown_output
+node release_notes_extractor.js $changelog_file_path $trim_markdown_output
 
 exit 0
